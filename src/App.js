c0fc87473cb66f715c62cec9b0dd640347a70820
Lester Arteaga <lesterarte@gmail.com>
2019-08-16 20:50:21 -0600

    add a complements and dependencies

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-      </header>
+      <button className="btn btn-primary">
+        hola tio
+
+      </button>
+      
     </div>
   );
 }
